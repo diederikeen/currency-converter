70dfed3ceea8108639c08d1e6d765dc32e55956f
Diederik Eenschooten <diederik@superinteractive.com>
2019-08-20 16:29:51 +0200

    Added comment to keys.js

--- src/js/helpers/calls/fetchFixer.js
+++ src/js/helpers/calls/fetchFixer.js
@@ -14,13 +14,14 @@ const buildURI = (obj) => {
   return uri;
 };
 
-function fetchFixer(uriParams, cbSucces, cbError) {
+const fetchFixer = (uriParams, cbSucces, cbError) => {
   const uri = buildURI(uriParams);
 
   return new Promise((resolve, reject) => {
     fetch(uri, { method: 'get' })
       .then((res) => res.json())
       .then((data) => {
+        // console.log(data);
         if (!data.success) {
           cbError(data.error);
           return;
@@ -30,6 +31,47 @@ function fetchFixer(uriParams, cbSucces, cbError) {
       })
       .catch((error) => reject(error));
   });
-}
+};
 
 export default fetchFixer;
+
+
+
+// import FixerKey from '../../secret/keys';
+
+// const buildURI = (obj) => {
+//   let uri = '';
+
+//   Object.keys(obj).forEach((key) => {
+//     if (key === 'endpoint') {
+//       uri = `http://data.fixer.io/api/${obj[key]}?access_key=${FixerKey}`;
+//     } else {
+//       uri += `&${key}=${obj[key]}`;
+//     }
+//   });
+
+//   return uri;
+// };
+
+// const FetchFixer = (uriParams, cbSucces, cbError) => {
+//   const uri = buildURI(uriParams);
+
+//   return new Promise((resolve, reject) => {
+//     fetch(uri, { method: 'get' })
+//       .then(res => res.json())
+//       .then((data) => {
+//         if (!data.success) {
+//           cbError(data.error);
+//           return;
+//         }
+//         resolve(data);
+//         cbSucces(data);
+//       })
+//       .catch(error => {
+//         reject(error);
+//         cbError(error);
+//       });
+//   });
+// };
+
+// export default FetchFixer;
--- src/js/secret/keys.js
+++ src/js/secret/keys.js
@@ -1,3 +1,4 @@
+// usually this would come from an environment variable but for ease of use/review I’ve left it in
 const fixerApiKey = '5277282ea5db9be03e38d90b4fc7e56f';
 
 export default fixerApiKey;
